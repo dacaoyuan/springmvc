6d5ad562a241a3e996a590987b64015a440057fc
ypk <2908363622@qq.com>
2017-09-11 11:40:02 +0800

    add upload file

--- src/main/java/com/shkj/spring/controller/CourseController.java
+++ src/main/java/com/shkj/spring/controller/CourseController.java
@@ -2,18 +2,20 @@ package com.shkj.spring.controller;
 
 import com.shkj.spring.model.Course;
 import com.shkj.spring.service.CourseService;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Map;
 
 @Controller
@@ -99,7 +101,7 @@ public class CourseController {
 
     //http://localhost:9090/courses/save
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String doSave(Course course) {
+    public String doSave(@ModelAttribute Course course) {
         String string = ReflectionToStringBuilder.toString(course);//能把传入的对象，里面包含的参数，全部显示成字符串
         log.info("doSave courseId={}", string);
 
@@ -108,6 +110,36 @@ public class CourseController {
         course.setCourseId(123);
 
         return "redirect:view2/" + course.getCourseId();
+    }
+
+
+    @RequestMapping(value = "/upload", method = RequestMethod.GET)
+    public String showUploadPage() {
+        return "course_admin/file";
+    }
+
+    @RequestMapping(value = "/doUpload", method = RequestMethod.POST)
+    public String doUploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+
+        if (file != null) {
+            String originalFilename = file.getOriginalFilename();
+
+            long fileSize = file.getSize();
+            log.info("fileSize={}", fileSize);
+
+            String fileContentType = file.getContentType();
+            log.info("fileContentType={}", fileContentType);
+
+            String fileName = file.getName();
+            log.info("fileName={}", originalFilename);
+            String s = new SimpleDateFormat("yyyy-MM-dd").format(System.currentTimeMillis());
+            FileUtils.copyInputStreamToFile(file.getInputStream(), new File("D://aa", s + originalFilename));
+
+
+        }
+
+
+        return "success";
     }
 
 
